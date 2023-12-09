6a74a916869be1f03ee9efdafa8ea16564a7db85
Sahithi-Sane <52241474+Sahithi-Sane@users.noreply.github.com>
2023-12-09 13:55:05 -0500

    Update Diabetes.py

--- Project/Diabetes.py
+++ Project/Diabetes.py
@@ -553,7 +553,6 @@ def model_analysis():
     elif  data_button == "LightGBM":
         with st.expander("Understand what LightGBM is and how it works"):
             st.write("LightGBM is a gradient boosting framework that efficiently trains decision tree ensembles. It employs a histogram-based approach for binning continuous features, reducing memory usage and speeding up training. LightGBM uses a leaf-wise tree growth strategy, optimizing for computational efficiency and scalability, making it particularly well-suited for large datasets.")
-        opt_parameters =  grid_search.best_params_
         model = GaussianNB() 
     else:
         st.error("Invalid model selection.")
